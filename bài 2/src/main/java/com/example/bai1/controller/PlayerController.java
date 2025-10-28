bb60ef299f18b83bc05320c478e37b4571f77f35
VanThanh34 <vvt1304@gmail.com>
2025-10-28 14:34:40 +0700

    Bài tập 2

---
+++ bài 2/src/main/java/com/example/bai1/controller/PlayerController.java
@@ -0,0 +1,38 @@
+package com.example.bai1.controller;
+
+import com.example.bai1.entity.Player;
+import com.example.bai1.service.IPlayerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("players")
+public class PlayerController {
+    private final IPlayerService iPlayerService;
+
+    public PlayerController(IPlayerService iPlayerService) {
+        this.iPlayerService = iPlayerService;
+    }
+
+    @GetMapping
+    public String listPlayer(Model model){
+        List<Player> playerList = iPlayerService.findAll();
+        model.addAttribute("playerList", playerList);
+        return "football_player/list";
+    }
+    @GetMapping("/detail/{id}")
+    public String detailPlayer(@PathVariable("id") int id, Model model){
+        model.addAttribute("player", iPlayerService.findById(id));
+        return "football_player/detail";
+    }
+    @PostMapping("/delete/{id}")
+    public String deletePlayer(@PathVariable("id") int id, RedirectAttributes redirectAttributes) {
+        iPlayerService.deleteById(id);
+        redirectAttributes.addFlashAttribute("mess", "Xoá cầu thủ thành công!");
+        return "redirect:/players";
+    }
+}
---
+++ bài 2/src/main/java/com/example/bai1/service/IPlayerService.java
@@ -0,0 +1,13 @@
+package com.example.bai1.service;
+
+import com.example.bai1.entity.Player;
+
+import java.util.List;
+
+public interface IPlayerService {
+    List<Player> findAll();
+
+    Player findById(int id);
+
+    void deleteById(int id);
+}
